6f11e0a1858ccff8e94542380549437e559890a2
Vmoney26 <vmoneyjiji@gmail.com>
2023-02-21 13:49:40 -0800

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - complete the absolute value of an integear
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
