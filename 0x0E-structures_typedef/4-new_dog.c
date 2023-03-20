cb8da47b54c17c161f415cca41ebf6f42ef9d9d3
Vmoney26 <vmoneyjiji@gmail.com>
2023-03-20 16:13:26 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,13 +24,13 @@ int _strlen(char *s)
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
- * to the buffer pointed to by victor
+ * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
- * Return: the pointer to victor
+ * Return: the pointer to dest
  */
-char *_strcpy(char *victor, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
@@ -42,11 +42,11 @@ char *_strcpy(char *victor, char *src)
 	}
 	for (i = 0; i < len; i++)
 	{
-		victor[i] = src[i];
+		dest[i] = src[i];
 	}
 
-	victor[i] = '\0';
-	return (victor);
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
